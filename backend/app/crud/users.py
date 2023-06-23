dc2472c3093ff8be505544bdc649697fe2b14846
wiskani <wiskani@icloud.com>
2023-06-22 22:42:52 -0400

    con un error al crear el primer usuario

--- backend/app/crud/users.py
+++ backend/app/crud/users.py
@@ -1,8 +1,25 @@
 from sqlalchemy.orm import Session
 from app.models.users import User
+from app.schemas.users import UsersCreate
 
 # Purpose: CRUD operations for users
 
+#Get if user exists
+async def get_first_user(db: Session) -> (User | None):
+    return db.query(User).first()
+
+# Get a user by email
 async def get_user_by_email(db: Session, email: str) -> (User | None):
     return db.query(User).filter(User.email == email).first()
+
+#Create a user
+async def create_user(db: Session, user: UsersCreate) -> User:
+    if get_user_by_email(db, user.email) is not None:
+        raise Exception("User already exists")
+    db_user: User = User(email=user.email, permissions=user.permissions, password=user.password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+ 
 
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+import getpass
 
-from app.db.database import engine
+from fastapi import FastAPI
+from sqlalchemy.orm import Session
+from pydantic import EmailStr, ValidationError
+
+from app.db.database import engine, SessionLocal
+from app.crud.users import get_first_user, create_user
+from app.schemas.users import UsersCreate
 
 import app.db.database as _database
 
@@ -13,3 +19,27 @@ _database.Base.metadata.create_all(bind=engine)
 app:FastAPI = FastAPI()
 
 app.include_router(_auth.router)
+
+# Create first user
+@app.on_event("startup")
+async def startup() -> None:
+    db: Session = SessionLocal()
+    if await get_first_user(db) is None:
+        id: int = 1
+        while True:
+            try:
+                email_str: str = input("Enter email: ")
+                email: EmailStr = EmailStr(email_str)
+                break
+            except ValidationError:
+                print("El correo es invalido por favor coloque un correo valido")
+        while True:
+            password: str = getpass.getpass("Enter password: ")
+            if 7 <= len(password) <= 30:
+                break
+            else:
+                print("La contraseña debe tener al menos 8 caracteres y menos de 30")
+        permissions: str = "admin"
+        user: UsersCreate = UsersCreate(id=id, email=email, permissions=permissions, password=password)
+        await create_user(db, user)
+        print("User created")
