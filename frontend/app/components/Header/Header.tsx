e3dd51886bff476236e3d210070ef5b5ad61473a
wiskani <wiskani@icloud.com>
2023-07-05 17:00:15 -0400

    algunas mejoras

--- frontend/app/components/Header/Header.tsx
+++ frontend/app/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import React, { useState } from "react";
-import { signOut, useSession } from "next-auth/react";
+import { signOut, signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation"; 
 import Link from "next/link";
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const router = useRouter();
     const { data: session } = useSession();
     const user = session?.user;
 
@@ -21,7 +23,23 @@ const Header = () => {
                         <img src="" className="mr-3 h-6 sm:h-9" alt="Logo" />
                         </a>
                 <div className="flex items-center lg:order-2">
-                    <a href="#" className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none ">Cerrar sesión</a>
+                    <>
+                    {user ? (
+                    <button
+                        onClick={() => signOut()}
+                        className='w-full text-white bg-red-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
+                    >
+                        Cerrar sesión
+                    </button>
+                    ) : (
+                    <button
+                        onClick={() => router.push("/login")}
+                        className='w-full text-white bg-emerald-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
+                    >
+                        Iniciar sesión
+                    </button>
+                    )}  
+                    </>
                     <button
                         onClick={handleToggle}
                         data-collapse-toggle="mobile-menu-2"
@@ -61,13 +79,18 @@ const Header = () => {
                 <div className={`${isOpen ? '' : 'hidden'} justify-between items-center w-full lg:flex lg:w-auto lg:order-1`} id="mobile-menu-2">
                     <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                         <li>
+                            {
+                                user?.permissions ==="admin" ? (
                             <a
                                 href="#"
-                                className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0"
-                                aria-current="page"
+                                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
                             >
                                 Personal
                             </a>
+                                ) : (
+                                    <>  </>
+                                )
+                            }
                         </li>
                         <li>
                             <a 
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,6 @@
 import { useSession  } from 'next-auth/react'
 import React from 'react';
 import { useRouter } from 'next/navigation';
-
 export default function HomePage() {
     const router = useRouter();
     const { data: session } = useSession();
@@ -11,13 +10,12 @@ export default function HomePage() {
         return (
             <div>
                 <h1>Home Page</h1>
-                <p>Signed in as {session.user.name}</p>
             </div>
         )
     }
     return (
         <div>
-            <button onClick={() => router.push('/login')}>Sign in</button>
+            <h1> Debes iniciar session</h1>
         </div>
     )
 }
