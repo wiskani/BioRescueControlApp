b0385f02354a1446977fc99e202b659187b50683
wiskani <wiskani@icloud.com>
2023-11-07 22:28:59 -0400

    sacando gbif a reptiles

--- backend/app/services/gbif/api_gbif.py
+++ backend/app/services/gbif/api_gbif.py
@@ -1,6 +1,7 @@
 from pygbif import species
 from typing import Optional, Dict
 from fastapi import HTTPException
+from pydantic import ValidationError
 
 from app.schemas.services import SpecieGbif
 
@@ -14,10 +15,16 @@ def get_species_suggestions(query:str, r:Optional[str])->dict:
 # Get species details
 def get_species_details(key:str)-> SpecieGbif| HTTPException:
     data = species.name_usage(key=key, data='all')
+    #Chek if class is a reptilia
+    if data['class'] == "Testudines" or data['class'] == "Squamata" or data['class'] == "Crocodilia" or data['class'] == "Sphenodontia":
+        raise HTTPException(status_code=404, detail='Class Reptilia does not can be registered in the system automatically')
     # check if data is SpecieGbif
     try :
         data = SpecieGbif(**data)
         return data
+    except ValidationError as e:
+        error_message = e.errors()
+        raise HTTPException(status_code=404, detail=error_message)
     except Exception as e:
         raise HTTPException(status_code=404, detail=e)
 
