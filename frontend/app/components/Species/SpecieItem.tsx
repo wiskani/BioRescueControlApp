3dd9855100b0443cb9d418027a19541bcf30dbeb
wiskani <wiskani@icloud.com>
2023-12-04 12:23:57 -0400

    implementado mapa de flora

--- frontend/app/components/Species/SpecieItem.tsx
+++ frontend/app/components/Species/SpecieItem.tsx
@@ -1,11 +1,12 @@
 
 const SpecieItem = (props: SpecieItemData) => {
   const imageUrl = props.images && props.images.length > 0 ? `http://localhost:8080${props.images[0].url}` : 'http://localhost:8080/static/images/species/no_imagen.svg';
+  const imageTitle = props.images && props.images.length > 0 ? props.images[0].atribute : 'No hay imagen';
   return (
     <section className="text-gray-700 body-font overflow-hidden bg-white">
       <div className="container px-5 py-5 mx-auto">
         <div className="mx-auto flex flex-wrap">
-            <img alt="specie" className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={imageUrl}/>
+            <img alt="specie" title={imageTitle} className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={imageUrl}/>
             <div className="w-full mt-6 ">
               <h1 className="text-gray-900 text-xl  title-font font-medium mb-1">Especie: {props.specie_name}</h1>
               <h2 className="text-sm title-font text-gray-500 tracking-widest">Genero: {props.genus_full_name}</h2>
