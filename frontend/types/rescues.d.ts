ffbe2fbc9a166cdd0203c12d18876630f21e8c72
wiskani <wiskani@icloud.com>
2023-12-27 00:15:14 -0400

    implementando link de rescates

---
+++ frontend/types/rescues.d.ts
@@ -0,0 +1,37 @@
+export {};
+
+declare global {
+        interface FloraRescueSpeciesData {
+                epiphyte_number: string; 
+                rescue_date: Date;
+                rescue_area_latitude: number;
+                rescue_area_longitude: number; 
+                specie_name: string | null; 
+                genus_name: string | null; 
+                family_name: string| null; 
+        }
+        
+        interface TransectHerpetoWithSpeciesData {
+                number: string; 
+                date_in: Date; 
+                date_out: Date; 
+                latitude_in: number; 
+                longitude_in: number; 
+                latitude_out: number; 
+                longitude_out: number; 
+                specie_names: string[]; 
+                total_rescue: number; 
+        }
+
+        interface RescueMammalsWithSpecieData {
+                cod: string; 
+                date: Date; 
+                longitude: number; 
+                latitude: number ;
+                observation: string|null; 
+                specie_name: string | null; 
+                genus_name: string | null; 
+        }
+
+}
+
