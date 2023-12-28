a51be09e1c5ef1ca12140609ef706575d40e555d
wiskani <wiskani@icloud.com>
2023-12-28 10:23:00 -0400

    implementando link de rescates

--- frontend/app/api/species/route.tsx
+++ frontend/app/api/species/route.tsx
@@ -13,4 +13,23 @@ export const SpeciesItem = async (props:Token): Promise<SpecieItemData[]> => {
   const data = await response.json();
   return data;
 }
+
+type RescuesSpecieData = FloraRescueSpeciesData | TransectHerpetoWithSpeciesData | RescueMammalsWithSpecieData 
+
+export const RescuesSpecie = async (props:Token): Promise<RescuesSpecieData[]> => { 
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + props.token ,
+    },
+  };
+  const response = await fetch(
+    'http://localhost:8080/api/join/rescues/species',
+    requestOptions
+  );
+  const data = await response.json();
+  return data;
+}
+
   
